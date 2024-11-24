4417cb7e243cc314fb0c2aebb79d554fde2b4b08
Speechrezz <thespeechlessmusic@gmail.com>
2024-11-24 09:58:54 -0800

    Updated inline macro

--- Source/ssimd/Register/RegisterNeon.hpp
+++ Source/ssimd/Register/RegisterNeon.hpp
@@ -16,15 +16,15 @@ struct Register<float, neon> : public RegisterOverloads<Register<float, neon>>
 	Register() = default;
 	SSIMD_INLINE Register(const float32x4_t& newData) : data(newData) {}
 
-	static inline constexpr size_t size() { return 4; }
-	static inline constexpr size_t alignment() { return 16; }
+	static SSIMD_INLINE constexpr size_t size() { return 4; }
+	static SSIMD_INLINE constexpr size_t alignment() { return 16; }
 
-	static inline Register<float, neon> loadAligned(const float* memory)
+	static SSIMD_INLINE Register<float, neon> loadAligned(const float* memory)
 	{
 		return { vld1q_f32(memory) };
 	}
 
-	inline void storeAligned(float* memory) const
+	SSIMD_INLINE void storeAligned(float* memory) const
 	{
 		vst1q_f32(memory, data);
 	}
@@ -33,17 +33,17 @@ struct Register<float, neon> : public RegisterOverloads<Register<float, neon>>
 
 // ---Arithmetic---
 
-inline Register<float, neon> add(const Register<float, neon>& reg1, const Register<float, neon>& reg2)
+SSIMD_INLINE Register<float, neon> add(const Register<float, neon>& reg1, const Register<float, neon>& reg2)
 {
 	return { vaddq_f32(reg1.data, reg2.data) };
 }
 
-inline Register<float, neon> sub(const Register<float, neon>& reg1, const Register<float, neon>& reg2)
+SSIMD_INLINE Register<float, neon> sub(const Register<float, neon>& reg1, const Register<float, neon>& reg2)
 {
 	return { vsubq_f32(reg1.data, reg2.data) };
 }
 
-inline Register<float, neon> mul(const Register<float, neon>& reg1, const Register<float, neon>& reg2)
+SSIMD_INLINE Register<float, neon> mul(const Register<float, neon>& reg1, const Register<float, neon>& reg2)
 {
 	return { vmulq_f32(reg1.data, reg2.data) };
 }
@@ -51,7 +51,7 @@ inline Register<float, neon> mul(const Register<float, neon>& reg1, const Regist
 
 // ---Trig---
 
-inline Register<float, neon> sin(const Register<float, neon>& reg)
+SSIMD_INLINE Register<float, neon> sin(const Register<float, neon>& reg)
 {
 	return generic::sin(reg);
 }
